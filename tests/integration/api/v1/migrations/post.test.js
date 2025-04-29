63d896f88401fd20b3876eed9883d22e4cc3afc1
rafael.boucher <simplicio.rsb@gmail.com>
2025-04-28 23:36:14 -0300

    refactor: make `api/v1/migrations` use `migrator` model

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -39,24 +39,3 @@ describe("POST /api/v1/migrations", () => {
     });
   });
 });
-
-describe("PUT /api/v1/migrations", () => {
-  describe("Anonymous user", () => {
-    test("Retrieving pending migrations", async () => {
-      const response = await fetch("http://localhost:3000/api/v1/migrations", {
-        method: "PUT",
-      });
-      expect(response.status).toBe(405);
-
-      const responseBody = await response.json();
-
-      expect(responseBody).toEqual({
-        name: "MethodNotAllowedError",
-        message: "Método não permitido para este endpoint.",
-        action:
-          "Verifique se o método HTTP enviado é válido para este endpoint.",
-        status_code: 405,
-      });
-    });
-  });
-});
